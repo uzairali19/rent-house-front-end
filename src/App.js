9f7214d93dd568ee1d72154b94d23f123528a7cc
Isabellanamutenda <namtendaisabella.com>
2022-02-18 00:44:00 +0300

    Add signup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DeleteListItems from './components/DeleteListItems';
+import Item from './components/Item';
+import Login from './components/Login';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Item />} />
+        <Route path="/deleteList" element={<DeleteListItems />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
-// import { Provider } from redux
+import 'bootstrap/dist/css/bootstrap.css';
+
+import store from './redux/configureStore';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
