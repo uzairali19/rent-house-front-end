d0d820bb5b7455b0f776cd2ce8280888f3b4b721
Isabellanamutenda <namtendaisabella.com>
2022-02-18 01:00:41 +0300

    Update the app.js file to iport the login from components

--- src/components/details/Button.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-
-const Button = ({ name }) => <button type="button">{name}</button>;
-
-Button.propTypes = {
-  name: PropTypes.string.isRequired,
-};
-
-export default Button;
