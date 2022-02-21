6076aa08da4b2032e981524da98926d8a35739bc
Elmar <L-mar@inbox.ru>
2022-02-21 16:37:39 +0400

    Create component structure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-// import { Provider } from redux
+import App from './components/App/App';
 
 ReactDOM.render(
   <React.StrictMode>
