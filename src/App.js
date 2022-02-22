6e4e1e49f5591bb14c5f43c42562197e35c5ecd3
Isabellanamutenda <namtendaisabella.com>
2022-02-22 09:51:40 +0300

    style the navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { 
+  BrowserRouter as Router, Switch, Route, Link 
+} from "react-router-dom";
 
-import Login from "./components/login.component";
-import SignUp from "./components/signup.component";
+import Login from './components/login.component';
+import SignUp from './components/signup.component';
 
 function App() {
   return (<Router>
