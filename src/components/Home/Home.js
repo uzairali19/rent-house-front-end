0112264908436e0165ed20da0f348d2f5d11c139
elmar8287 <L-mar@inbox.ru>
2022-02-23 13:21:10 +0400

    Fix linter issue

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
 
@@ -7,32 +7,32 @@ const houseLogo = require('./house-view.png');
 const Home = () => {
   return (
     <div>
-        <div className="item">
-            <img src={houseLogo} alt="House view" />
-            <h5>House #1</h5>
-            <p>Some short description or could be removed.</p>
-            <Link to={"/house-detail"}>
-            <button type="button">See house</button>
-            </Link>
-        </div>
-        <div className="item">
-            <img src={houseLogo} alt="House view" />
-            <h5>House #1</h5>
-            <p>Some short description or could be removed.</p>
-            <Link to={"/house-detail"}>
-            <button type="button">See house</button>
-            </Link>
-        </div>
-        <div className="item">
-            <img src={houseLogo} alt="House view" />
-            <h5>House #1</h5>
-            <p>Some short description or could be removed.</p>
-            <Link to={"/house-detail"}>
-            <button type="button">See house</button>
-            </Link>
-        </div>
+      <div className="item">
+          <img src={houseLogo} alt="House view" />
+          <h5>House #1</h5>
+          <p>Some short description or could be removed.</p>
+          <Link to="/house-detail">
+          <button type="button">See house</button>
+          </Link>
+      </div>
+      <div className="item">
+          <img src={houseLogo} alt="House view" />
+          <h5>House #1</h5>
+          <p>Some short description or could be removed.</p>
+          <Link to="/house-detail">
+          <button type="button">See house</button>
+          </Link>
+      </div>
+      <div className="item">
+          <img src={houseLogo} alt="House view" />
+          <h5>House #1</h5>
+          <p>Some short description or could be removed.</p>
+          <Link to="/house-detail">
+          <button type="button">See house</button>
+          </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
