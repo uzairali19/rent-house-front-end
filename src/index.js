7bd24858bcd9af71b184add6b8902d4967228200
Uzair Ali <uzairali19b@gmail.com>
2022-02-23 11:42:37 +0500

    initialize axios and redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
-// import { Provider } from redux
+import store from './redux/configStore';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
