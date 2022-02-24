9592200179becaeb42eca9319ab5e58f1c25dc68
Elmar <L-mar@inbox.ru>
2022-02-24 17:33:23 +0400

    Add delete button in house detail view

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../Header';
 
 const Favorites = () => (
   <div>
     <Header />
+    <div className="item">
+      <h5>House #1</h5>
+      <p>Spacious 2 bedroom, 1 bathroom apartment in Lakeview,available</p>
+      <Link to="/house-detail">
+        <button type="button" className="button">
+          <span>See house</span>
+        </button>
+      </Link>
+    </div>
   </div>
 );
 
