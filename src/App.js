69c19d3ca0e0e5b36d417c373edfd3e16de8148c
Isabellanamutenda <namtendaisabella.com>
2022-02-24 13:18:24 +0300

    connect the signup page with the home page

--- src/App.js
+++ src/App.js
@@ -1,49 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link, Routes, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import NavPanel from './components/NavPanel';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
+import Header from './components/Header';
 
-import Login from './components/Auth/login.component';
-import SignUp from './components/Auth/signup.component';
 import Home from './components/Home/Home';
 import HouseDetail from './components/Home/House-detail';
 
 const App = () => (
   <BrowserRouter>
-    <Routes>
-      <div className="App">
-        <NavPanel />
-        {/* <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-          <div className="container">
-            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/sign-in">
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/sign-up">
-                    Sign up
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav> */}
-        <div className="auth-wrapper">
-          <div className="auth-inner">
-            <Route exact path="/" element={<Login />} />
-            <Route path="/sign-in" element={<Login />} />
-            <Route path="/sign-up" element={<SignUp />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/house-detail" element={<HouseDetail />} />
-          </div>
-        </div>
-      </div>
-    </Routes>
-  </BrowserRouter>
+  <Header />
+  <Routes>
+    <Route path="/" element={<Login />} />
+    <Route path="sign-in" element={<Login />} />
+    <Route path="sign-up" element={<SignUp />} />
+    <Route path="/home" element={<Home />} />
+    <Route path="/house-detail" element={<HouseDetail />} />
+  </Routes>
+</BrowserRouter>
 );
 
 export default App;
