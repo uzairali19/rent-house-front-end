a114b112a9618bf6526a4fd52f855809bf0808fa
Uzair Ali <uzairali19b@gmail.com>
2022-02-25 11:05:05 +0500

    update houseReducer

--- src/redux/reducers/houseReducer.js
+++ src/redux/reducers/houseReducer.js
@@ -1,4 +1,11 @@
-import { GET_HOUSES } from '../types';
+import {
+  DELETE_HOUSES,
+  DELETE_RESERVATION,
+  GET_HOUSES,
+  GET_RESERVATIONS,
+  POST_HOUSES,
+  POST_RESERVATION
+} from '../types';
 
 const initialState = [];
 
@@ -6,6 +13,16 @@ const houseReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_HOUSES:
       return action.payload;
+    case POST_HOUSES:
+      return [...state, action.payload];
+    case DELETE_HOUSES:
+      return state.filter((house) => house.id !== action.payload);
+    case GET_RESERVATIONS:
+      return action.payload;
+    case POST_RESERVATION:
+      return [...state, action.payload];
+    case DELETE_RESERVATION:
+      return state.filter((reservation) => reservation.id !== action.payload);
     default:
       return state;
   }
